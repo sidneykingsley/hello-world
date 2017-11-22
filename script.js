1b248c096e890992d06516bdf99a775d8b2e15cb
(s) Sidney Kingsley <sakingsley@pm7-0100.uopnet.plymouth.ac.uk>
2017-11-22 13:14:26 +0000

    HELLO WORLD

---
+++ script.js
@@ -0,0 +1,10 @@
+function textcolorchange(x) {
+  x.style.color = "#faf";
+};
+function textcolorrevert(x) {
+  x.style.color = "white";
+};
+
+function fitscreen() {
+  document.getElementById("allcontent").style.height = window.screen.height + "px";
+}
